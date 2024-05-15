9e5aa4178128623f414cab05708340d5d2c340d2
Zhile Zhu <zhuzhile08@gmail.com>
2024-05-15 18:29:43 +0200

    Add generic test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
+#include <Common/FileSystem.h>
+#include <Common/Logger.h>
+#include <Common/JSON.h>
 
+#include <matplot/matplot.h>
 
-int main() {
+int main(int, char** argv) {
+	lstd::initLoggingSystem();
+	lstd::initFileSystem(argv);
 
+	lstd::Json data = lstd::Json::parse(lstd::StringStream("data/data.json").data());
+
+	lstd::log::debug(data.stringifyPretty());
 }
